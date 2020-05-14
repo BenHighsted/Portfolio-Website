98d42c567eb52d11518f7e0786c670c263a7c743
Ben Highsted <49290301+BenHighsted@users.noreply.github.com>
2020-05-15 01:49:13 +1200

    Updated to work with revamped website

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
+/** THOUGHTS ON ADAPTING FOR NEW WEBSITE:
+ * As this could be viewed by potential employers, i want the divs to have appropriate names so
+ * Instead of doing the 'div + i' method, ill instead have an array hard coded of all the primary divs
+ * Hopefully shouldn't be too hard. Think I should get this going before adding any more to the document.
+ */
+
+var pages = ["home", "about", "academic", "projects", "contact"];
+
+
 /** Takes the user to the requested div. Scrolls past any other divs on the way. */
 function autoScrollTo(el) {
 
@@ -11,17 +21,18 @@ function autoScrollTo(el) {
 
 /** Shows all divs for the animation to take place */
 function showDivs(){
-    var amountOfDivs = 4;
-    for(var i = 0; i < amountOfDivs; i++){
-        document.getElementById("div" + i).style.display = "block";
+    var amountOfDivs = 5;
+    for(var i = 0; i < 4; i++){
+        var div = pages[i];
+        document.getElementById(div).style.display = "block";
     }
 }
 
 /** Hides all divs except for the one the user wishes to view  */
 function hideDivs(el){
     setTimeout(function(){
-        for(var i = 0; i < 4; i++){
-            var div = "div" + i;
+        for(var i = 0; i < 5; i++){
+            var div = pages[i];
             if(el != div){
                 document.getElementById(div).style.display = "none";
             }
